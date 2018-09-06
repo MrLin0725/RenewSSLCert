3e0cc408a084f34f40baf0705df50675b55380f7
MrLin <736607939@qq.com>
2018-09-06 17:55:42 +0800

    style: 添加注释

--- authenticator.sh
+++ authenticator.sh
@@ -2,5 +2,9 @@
 
 subDomain="_acme-challenge"
 
+# 通过环境变量 RENEW_DOMAIN 获取需要续期的域名
+# CERTBOT_VALIDATION 是certbot设置的变量
 $(which python3) handler.py ${RENEW_DOMAIN} ${subDomain} ${CERTBOT_VALIDATION}
+
+# 等待DNS解析生效
 sleep 30
