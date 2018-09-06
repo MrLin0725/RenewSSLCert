206174a9a34221d6d0c94c0b86c7d5b72e427c7d
MrLin <736607939@qq.com>
2018-09-06 17:30:31 +0800

    fix: 参数错误

--- handler.py
+++ handler.py
@@ -3,4 +3,4 @@ import sys
 from app.tencent.request import create_record
 
 if __name__ == '__main__':
-    create_record(domain=sys.argv[0], sub_domain=sys.argv[1], value=sys.argv[2])
+    create_record(domain=sys.argv[1], sub_domain=sys.argv[2], value=sys.argv[3])
