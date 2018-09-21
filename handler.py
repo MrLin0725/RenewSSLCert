9cc0156b0803049270e7bd4aabef601a72ad7b61
MrLin <736607939@qq.com>
2018-09-21 18:25:39 +0800

    feat: 根据参数续期证书

--- handler.py
+++ handler.py
@@ -1,6 +1,6 @@
 import sys
 
-from app.tencent.request import create_record
+from tencentcloud.core import TencentCloudRequester
 
 if __name__ == '__main__':
     # 获取 authenticator.sh 传递的三个参数：域名，子域名，记录值
@@ -8,8 +8,6 @@ if __name__ == '__main__':
     sub_domain = sys.argv[2]
     value = sys.argv[3]
 
-    # TODO: 获取解析记录，判断是否有已经存在的记录
-    # TODO: 删除存在的记录
-
     # 创建解析记录
-    create_record(domain=domain, sub_domain=sub_domain, value=value)
+    requester = TencentCloudRequester(domain=domain, sub_domain=sub_domain, value=value)
+    requester.start()
