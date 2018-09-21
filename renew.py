7c43b23fb68bf918bef32969c9db193d9c2b6a45
MrLin <736607939@qq.com>
2018-09-21 18:49:07 +0800

    feat: 删除 --dry-run 测试参数

--- renew.py
+++ renew.py
@@ -29,9 +29,8 @@ if __name__ == '__main__':
         logger.info('Renew {} SSL certificate'.format(domain))
         # 设置环境变量
         os.environ['RENEW_DOMAIN'] = domain
-        # FIXME: --dry-run 测试阶段
         subprocess.call(
-            '{} renew --cert-name {} --manual-auth-hook {} --dry-run'.format(
+            '{} renew --cert-name {} --manual-auth-hook {}'.format(
                 certbot_path, domain, authenticator_path
             ),
             shell=True
