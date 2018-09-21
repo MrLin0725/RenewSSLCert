956732d9904b14a860cf38d6a9403a020cdc7959
MrLin <736607939@qq.com>
2018-09-21 18:26:02 +0800

    feat: 添加日志输出

--- renew.py
+++ renew.py
@@ -1,15 +1,19 @@
 import os
 import subprocess
 
-from app.tools.params import get_params
+from tencentcloud.utils import get_params
+from tencentcloud.logs import Logger
 
 if __name__ == '__main__':
+    logger = Logger()
+
     (certbot_dir, domains) = get_params('CERTBOT_DIR', 'DOMAINS')
 
     # certbot-auto 绝对路径
     certbot_path = os.path.join(certbot_dir, 'certbot-auto')
     if not os.path.isfile(certbot_path):
-        raise FileNotFoundError('Can not found certbot-auto')
+        logger.critical('Can not find certbot')
+        raise FileNotFoundError(certbot_path + ' not exist')
 
     # authenticator 脚本绝对路径
     authenticator_path = os.path.join(
@@ -17,10 +21,12 @@ if __name__ == '__main__':
         'authenticator.sh'
     )
     if not os.path.isfile(authenticator_path):
-        raise FileNotFoundError('Can not found authenticator.sh')
+        logger.critical('Can not find authenticator.sh')
+        raise FileNotFoundError(authenticator_path + ' not exist')
 
     # 多个域名
     for domain in domains:
+        logger.info('Renew SSL {} certificate'.format(domain))
         # 设置环境变量
         os.environ['RENEW_DOMAIN'] = domain
         # FIXME: --dry-run 测试阶段
