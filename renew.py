779f9f3cdd6d3f829790d0c10ded1832f69f439e
MrLin <736607939@qq.com>
2018-09-25 15:32:05 +0800

    feat: 调用清理脚本

--- renew.py
+++ renew.py
@@ -24,14 +24,23 @@ if __name__ == '__main__':
         logger.critical('Can not find authenticator.sh')
         raise FileNotFoundError(authenticator_path + ' not exist')
 
+    # cleanup 脚本路径
+    cleanup_path = os.path.join(
+        os.path.split(os.path.realpath(__file__))[0],
+        'cleanup.sh'
+    )
+    if not os.path.isfile(cleanup_path):
+        logger.critical('Can not find cleanup.sh')
+        raise FileNotFoundError(cleanup_path + ' not exist')
+
     # 多个域名
     for domain in domains:
         logger.info('Renew {} SSL certificate'.format(domain))
         # 设置环境变量
         os.environ['RENEW_DOMAIN'] = domain
         subprocess.call(
-            '{} renew --cert-name {} --manual-auth-hook {}'.format(
-                certbot_path, domain, authenticator_path
+            '{} renew --cert-name {} --manual-auth-hook {} --manual-cleanup-hook {}'.format(
+                certbot_path, domain, authenticator_path, cleanup_path
             ),
             shell=True
         )
