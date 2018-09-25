861ade04bc708c51e2ca5820fe8f44b641020574
MrLin <736607939@qq.com>
2018-09-25 16:01:24 +0800

    feat: 测试参数

--- config.example.py
+++ config.example.py
@@ -11,10 +11,13 @@ class Config(object):
     ]
     # certbot-auto 目录绝对路径
     CERTBOT_DIR = '/root'
+    # 是否需要测试
+    CERTBOT_TEST = True
 
     # 腾讯云解析 SecretId
     TENCENT_CNS_SECRETID = ''
     # 腾讯云解析 SecretKey
     TENCENT_CNS_SECRETKEY = ''
 
+    # 日志路径
     LOG_PATH = os.path.join(os.path.split(os.path.realpath(__file__))[0], 'renew.log')
